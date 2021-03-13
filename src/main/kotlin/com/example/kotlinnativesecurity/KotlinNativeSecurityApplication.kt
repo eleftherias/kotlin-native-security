0c193f658bd24058c2e563864f98e00692067560
Eleftheria Stein <eleftheria.kousathana@gmail.com>
2021-03-13 14:15:23 +0100

    Kotlin DSLs

--- src/main/kotlin/com/example/kotlinnativesecurity/KotlinNativeSecurityApplication.kt
+++ src/main/kotlin/com/example/kotlinnativesecurity/KotlinNativeSecurityApplication.kt
@@ -2,57 +2,48 @@ package com.example.kotlinnativesecurity
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
+import org.springframework.context.support.beans
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.web.servlet.invoke
 import org.springframework.security.core.userdetails.User
-import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.provisioning.InMemoryUserDetailsManager
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.servlet.function.router
 
 @SpringBootApplication
 class KotlinNativeSecurityApplication
 
 fun main(args: Array<String>) {
-	runApplication<KotlinNativeSecurityApplication>(*args)
-}
-
-@RestController
-class BaseController {
-	@GetMapping("/hello")
-	fun hello(): String {
-		return "Hello! Happy Tuesday!"
-	}
-
-	@GetMapping("/talk/info")
-	fun info(): String {
-		return "Turning Kotlin Applications into Secure Native Executables on March 16, 2021."
-	}
-}
-
-@EnableWebSecurity
-class SecurityConfig : WebSecurityConfigurerAdapter() {
-
-    override fun configure(http: HttpSecurity?) {
-		http {
-			authorizeRequests {
-				authorize("/hello", permitAll)
-				authorize(anyRequest, authenticated)
-			}
-			httpBasic { }
-		}
-    }
-
-    @Bean
-    fun users(): UserDetailsService {
-        return InMemoryUserDetailsManager(
-            User.withUsername("user")
-                .password("{bcrypt}\$2a\$10\$CvFWWoyao/V1zk1c14/M7OwUOymy3lT7eXTCu6w4ERT7OHrpOYtDq")
-                .roles("USER")
-                .build()
+    runApplication<KotlinNativeSecurityApplication>(*args) {
+        addInitializers(
+                beans {
+                    bean {
+                        router {
+                            GET("/hello") { ok().body("Hello! Happy Tuesday!") }
+                            "/talk".nest {
+                                GET("/info") { ok().body("Turning Kotlin Applications into Secure Native Executables on March 16, 2021.") }
+                            }
+                        }
+                    }
+                    bean {
+                        InMemoryUserDetailsManager(
+                                User.withUsername("user")
+                                        .password("{bcrypt}\$2a\$10\$CvFWWoyao/V1zk1c14/M7OwUOymy3lT7eXTCu6w4ERT7OHrpOYtDq")
+                                        .roles("USER")
+                                        .build()
+                        )
+                    }
+                    bean {
+                        val http = ref<HttpSecurity>()
+                        http {
+                            authorizeRequests {
+                                authorize("/hello", permitAll)
+                                authorize(anyRequest, authenticated)
+                            }
+                            httpBasic { }
+                        }
+                        http.build()
+                    }
+                }
         )
     }
 }
