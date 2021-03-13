b22da21d6bdbfa0c9b5b49c3f65945b1857db715
Eleftheria Stein <eleftheria.kousathana@gmail.com>
2021-03-13 12:24:40 +0100

    Custom user

--- src/main/kotlin/com/example/kotlinnativesecurity/KotlinNativeSecurityApplication.kt
+++ src/main/kotlin/com/example/kotlinnativesecurity/KotlinNativeSecurityApplication.kt
@@ -2,6 +2,11 @@ package com.example.kotlinnativesecurity
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.provisioning.InMemoryUserDetailsManager
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -23,4 +28,17 @@ class BaseController {
 	fun info(): String {
 		return "Turning Kotlin Applications into Secure Native Executables on March 16, 2021."
 	}
+}
+
+@EnableWebSecurity
+class SecurityConfig {
+    @Bean
+    fun users(): UserDetailsService {
+        return InMemoryUserDetailsManager(
+            User.withUsername("user")
+                .password("{bcrypt}\$2a\$10\$CvFWWoyao/V1zk1c14/M7OwUOymy3lT7eXTCu6w4ERT7OHrpOYtDq")
+                .roles("USER")
+                .build()
+        )
+    }
 }
